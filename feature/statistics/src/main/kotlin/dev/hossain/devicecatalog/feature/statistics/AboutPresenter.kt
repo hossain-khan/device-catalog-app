65cb6435e19eae1a4242bd0fba11e630004f370c
Hossain Khan <hello@hossain.dev>
2025-12-07 18:58:57 -0500

    Rename ExampleAppVersionService to AppVersionService and remove example files
    - Renamed ExampleAppVersionService to AppVersionService in core:common
    - Updated all references across AboutPresenter, DeviceStatsPresenter
    - Removed example email-related files:
      - ExampleEmailRepository.kt
      - ExampleEmailValidator.kt
      - ExampleAppBindings.kt (DI bindings)
    - Removed example Circuit screen files:
      - ExampleInboxScreen.kt
      - ExampleEmailDetailsScreen.kt
    - Removed duplicate ExampleAppVersionService.kt from app module
    - Cleaned up example code that was meant as templates

--- feature/statistics/src/main/kotlin/dev/hossain/devicecatalog/feature/statistics/AboutPresenter.kt
+++ feature/statistics/src/main/kotlin/dev/hossain/devicecatalog/feature/statistics/AboutPresenter.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.presenter.Presenter
-import dev.hossain.devicecatalog.core.common.ExampleAppVersionService
+import dev.hossain.devicecatalog.core.common.AppVersionService
 import dev.hossain.devicecatalog.feature.settings.DeveloperSettingsScreenCircuit
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.Assisted
@@ -14,7 +14,7 @@ import dev.zacsweers.metro.AssistedInject
 @AssistedInject
 class AboutPresenter(
     @Assisted private val navigator: Navigator,
-    private val appVersionService: ExampleAppVersionService,
+    private val appVersionService: AppVersionService,
 ) : Presenter<AboutScreen.State> {
     @Composable
     override fun present(): AboutScreen.State =
