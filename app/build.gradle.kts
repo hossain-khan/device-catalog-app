a3454483f794bfd0c38f2fd1a45d3b2e25a17224
Hossain Khan <hello@hossain.dev>
2025-12-08 21:34:19 -0500

    [BUMP] App version for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 
     defaultConfig {
         applicationId = "dev.hossain.devicecatalog"
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.4.0"
 
         // Read key or other properties from local.properties
         val localProperties =
